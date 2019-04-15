9bda1b8928f5d8ccbbee5311e70735d05aee07be
Nicolas Debray <nicdebray@gmail.com>
2019-04-15 17:16:29 +0200

    finetuned js result rendement

--- app/javascript/packs/estimator.js
+++ app/javascript/packs/estimator.js
@@ -1,20 +1,12 @@
-const estimatorResult = document.getElementById('estimator-result');
-const estimatorCheckBtn = document.getElementById('estimator-checker');
-
-
 const estimator = () => {
   const monthlyRent = parseInt(document.getElementById('monthly-rent').value);
   const purchasePrice = parseInt(document.getElementById('purchase-price').value);
   const extraWorks = parseInt(document.getElementById('extra-works').value);
-  const netReturn = (monthlyRent*10)/((purchasePrice*1.15)+extraWorks);
   const estimatorResult = document.getElementById('estimator-result');
-
-  return estimatorResult.innerText = netReturn;
+  const netReturn = (monthlyRent*10)/((purchasePrice*1.15)+extraWorks);
+  return estimatorResult.innerText = `${Math.round(netReturn*10000) / 100} %`;
 }
 
 
-
+const estimatorCheckBtn = document.getElementById('estimator-checker');
 estimatorCheckBtn.addEventListener('click', estimator);
-
-
-
