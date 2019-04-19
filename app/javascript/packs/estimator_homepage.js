1f586002171db0ef6b1391c6cb5211dc11ddedeb
Nicolas Debray <nicdebray@gmail.com>
2019-04-19 18:06:36 +0200

    added mutliple calcultator js nav and calcul

--- app/javascript/packs/estimator_homepage.js
+++ app/javascript/packs/estimator_homepage.js
@@ -1,4 +1,7 @@
-const estimatorHomepage = () => {
+
+// estimator rental yield
+
+const estimatorRentalYield = () => {
   const purchasePrice = parseInt(document.getElementById('purchase-price-input-estimator').value);
   const monthlyRent = parseInt(document.getElementById('monthly-rent-input-estimator').value);
   const extraWorks = parseInt(document.getElementById('extra-works-input-estimator').value);
@@ -12,12 +15,12 @@ const estimatorHomepage = () => {
   estimatorResult10.innerText = `Based on 10 months rent ${Math.round(netReturn10*10000) / 100} %`;
   estimatorResult11.innerText = `Based on 11 months rent ${Math.round(netReturn11*10000) / 100} %`;
   estimatorResult12.innerText = `Based on 12 months rent ${Math.round(netReturn12*10000) / 100} %`;
-  estimatorResult.insertAdjacentHTML('beforeend', `<i class="fas fa-ban" id="estimator-eraser"></i>`);
-  const estimatorEraser = document.getElementById('estimator-eraser');
-  estimatorEraser.addEventListener('click', estimatorClear);
-}
+  estimatorResult.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle" id="estimator-eraser"></i>`);
+  const estimatorEraser = document.querySelector('#estimator-eraser');
+  estimatorEraser.addEventListener('click', estimatorRentalYieldClear);
+};
 
-const estimatorClear = () => {
+const estimatorRentalYieldClear = () => {
   const estimatorResult10 = document.getElementById('result-estimator10');
   const estimatorResult11 = document.getElementById('result-estimator11');
   const estimatorResult12 = document.getElementById('result-estimator12');
@@ -26,10 +29,90 @@ const estimatorClear = () => {
   estimatorResult11.innerText = ``;
   estimatorResult12.innerText = ``;
   estimatorEraser.parentNode.removeChild(estimatorEraser);
-}
+};
+
+
+// estimator monthly rent
+
+const estimatorMonthlyRent = () => {
+  const purchasePrice = parseInt(document.getElementById('purchase-price-input-estimator-mr').value);
+  const extraWorks = parseInt(document.getElementById('extra-works-input-estimator-mr').value);
+  const rentalYield = parseInt(document.getElementById('rental-yield-input-estimator-mr').value);
+  const estimatorResult = document.getElementById('result-estimator');
+  const estimatorResult10 = document.getElementById('result-estimator10');
+  const estimatorResult11 = document.getElementById('result-estimator11');
+  const estimatorResult12 = document.getElementById('result-estimator12');
+  const monthlyRent10 = (((purchasePrice*1.15)+extraWorks)*(rentalYield/100))/10;
+  const monthlyRent11 = (((purchasePrice*1.15)+extraWorks)*(rentalYield/100))/11;
+  const monthlyRent12 = (((purchasePrice*1.15)+extraWorks)*(rentalYield/100))/12;
+  estimatorResult10.innerText = `Based on 10 months rent ${Math.round(monthlyRent10)} €`;
+  estimatorResult11.innerText = `Based on 11 months rent ${Math.round(monthlyRent11)} €`;
+  estimatorResult12.innerText = `Based on 12 months rent ${Math.round(monthlyRent12)} €`;
+  estimatorResult.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle" id="estimator-eraser"></i>`);
+  const estimatorEraser = document.querySelector('#estimator-eraser');
+  estimatorEraser.addEventListener('click', estimatorRentalYieldClear);
+};
+
+
+// estimator monthly rent
+
+const estimatorPurchasePrice = () => {
+  const monthlyRent = parseInt(document.getElementById('monthly-rent-input-estimator-pp').value);
+  const extraWorks = parseInt(document.getElementById('extra-works-input-estimator-pp').value);
+  const rentalYield = parseInt(document.getElementById('rental-yield-input-estimator-pp').value);
+  const estimatorResult = document.getElementById('result-estimator');
+  const estimatorResult10 = document.getElementById('result-estimator10');
+  const estimatorResult11 = document.getElementById('result-estimator11');
+  const estimatorResult12 = document.getElementById('result-estimator12');
+  const purchasePrice10 = (((monthlyRent*10)/(rentalYield/100))-extraWorks)/1.15;
+  const purchasePrice11 = (((monthlyRent*11)/(rentalYield/100))-extraWorks)/1.15;
+  const purchasePrice12 = (((monthlyRent*12)/(rentalYield/100))-extraWorks)/1.15;
+  estimatorResult10.innerText = `Based on 10 months rent ${Math.round(purchasePrice10)} €`;
+  estimatorResult11.innerText = `Based on 11 months rent ${Math.round(purchasePrice11)} €`;
+  estimatorResult12.innerText = `Based on 12 months rent ${Math.round(purchasePrice12)} €`;
+  estimatorResult.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle" id="estimator-eraser"></i>`);
+  const estimatorEraser = document.querySelector('#estimator-eraser');
+  estimatorEraser.addEventListener('click', estimatorRentalYieldClear);
+};
+
 
 const estimatorCheckBtn = document.getElementById('estimator-checker');
-estimatorCheckBtn.addEventListener('click', estimatorHomepage);
+estimatorCheckBtn.addEventListener('click', estimatorPurchasePrice);
+
+
+// test
+// const estimatorCheckBtn = document.getElementById('estimator-checker');
+// estimatorCheckBtn.addEventListener('click', estimatorPurchasePrice) () => {
+//   if() {}
+//     else if () {}
+//   else {estimatorPurchasePrice}
+//     });
+
+
+
+
+
+// select estimator and show correct inputs
+
+const hideAllEstimator = () => {
+  const estimatorSelection = document.querySelectorAll('.estimator-inputs');
+  estimatorSelection.forEach((estimator) => {
+    estimator.classList.add('hidden');
+  });
+};
+
+
+const estimatorSelection = document.querySelectorAll('.description-estimator');
+
+
+
+estimatorSelection.forEach((estimator) => {
+  estimator.addEventListener('click', (event) => {
+    hideAllEstimator();
+    const classEstimator = `.${event.currentTarget.innerText[0]}`;
+    document.querySelector(`${classEstimator}`).classList.remove('hidden');
+  });
+});
 
 
 // default value to zero removed/added when focus in out
@@ -47,3 +130,5 @@ formInput.forEach((field) => {
       };
     });
 });
+
+
