732c1b101109b45b720afb99ba62673f9e618506
Nicolas Debray <nicdebray@gmail.com>
2019-04-21 21:45:28 +0200

    finetuned icons switch and hidden js

--- app/javascript/packs/after_load_menu_all.js
+++ app/javascript/packs/after_load_menu_all.js
@@ -1,11 +1,20 @@
 const showHideMenu = () => {
   const menu = document.querySelector('.main-menu');
   menu.classList.toggle('hidden');
+  };
+
+const switchIconMenu = () => {
+  const menuIcons = document.querySelectorAll('.navbar-main-burger .navbar-main-icon');
+  menuIcons.forEach((icon) => {
+    icon.classList.toggle('hidden');
+  });
 };
 
-const burgerMenu = document.querySelector('.navbar-main-burger');
+const burgerMenu = document.querySelectorAll('.navbar-main-burger .navbar-main-icon');
 
-burgerMenu.addEventListener('click', () => {
-  console.log('pizza');
-  showHideMenu()
+burgerMenu.forEach((icon) => {
+  icon.addEventListener('click', () => {
+    switchIconMenu();
+    showHideMenu();
+  });
 });
