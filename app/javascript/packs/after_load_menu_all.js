2a89c7620acfc6b5d180e7a783a5fb41b1142a35
Nicolas Debray <nicdebray@gmail.com>
2019-04-22 21:34:00 +0200

    refactored js menu

--- app/javascript/packs/after_load_menu_all.js
+++ app/javascript/packs/after_load_menu_all.js
@@ -1,16 +1,16 @@
+const menu = document.querySelector('.main-menu');
+const burgerMenu = document.querySelectorAll('.navbar-main-burger .navbar-main-icon');
+
+
 const showHideMenu = () => {
-  const menu = document.querySelector('.main-menu');
   menu.classList.toggle('hidden');
   };
 
 const switchIconMenu = () => {
-  const menuIcons = document.querySelectorAll('.navbar-main-burger .navbar-main-icon');
-  menuIcons.forEach((icon) => {
+  burgerMenu.forEach((icon) => {
     icon.classList.toggle('hidden');
   });
 };
-
-const burgerMenu = document.querySelectorAll('.navbar-main-burger .navbar-main-icon');
 
 burgerMenu.forEach((icon) => {
   icon.addEventListener('click', () => {
