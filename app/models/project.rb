d96505356f323183a806c0899c02d9eb846c9148
Nicolas Debray <nicdebray@gmail.com>
2019-04-25 12:17:53 +0200

    styled cards project form new. Added validations to avoid nomethod

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,14 @@
 class Project < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :purchase_price, presence: true
+  validates :monthly_rent, presence: true
+  validates :extra_works, presence: true
+
 
   def rental_yield
-    ((self.monthly_rent * 10) / ((self.purchase_price * 1.15) + self.extra_works)*100).round(2)
+    ((self.monthly_rent * 10) / ((self.purchase_price * 1.15) + self.extra_works) * 100).round(2)
   end
 
 end
