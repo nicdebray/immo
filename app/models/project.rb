13939b9ff0031349a4b5a7659976cdaa377c8a42
Nicolas Debray <nicdebray@gmail.com>
2019-05-01 00:24:09 +0200

    fixed the return formula

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,27 +6,31 @@ class Project < ApplicationRecord
   validates :monthly_rent, presence: true
   validates :extra_works, presence: true
 
-  def acquisition_value
+  def acquisition_full_price
     ((self.purchase_price * 1.15) + self.extra_works)
   end
 
+  def acquisition_value
+    self.purchase_price + self.extra_works
+  end
+
   def acquisition_cash
-    acquisition_value / 3
+    acquisition_full_price / 3
   end
 
   def rental_yield
-    ((self.monthly_rent * 10) / ((self.purchase_price * 1.15) + self.extra_works) * 100).round(2)
+    ((self.monthly_rent * 10) / acquisition_full_price * 100).round(2)
   end
 
   def capital_gain_10(rate: 0.03, years: 10)
-    (self.acquisition_value * (((1 + rate)**years)) - self.acquisition_value).round(0)
+    (self.acquisition_value * (((1 + rate)**years)) - self.acquisition_full_price).round(0)
   end
 
   def capital_gain_15(rate: 0.03, years: 15)
-    (self.acquisition_value * (((1 + rate)**years)) - self.acquisition_value).round(0)
+    (self.acquisition_value * (((1 + rate)**years)) - self.acquisition_full_price).round(0)
   end
 
   def project_return_15
-    (((acquisition_value + capital_gain_15) - acquisition_cash) / acquisition_cash).round(2)
+    (((acquisition_value + capital_gain_15) - acquisition_cash) / acquisition_cash).round(2) + 1
   end
 end
