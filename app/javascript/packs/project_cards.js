3c0fe9fa3d5d2408ff23b6bed975e305de0dc3c9
Nicolas Debray <nicdebray@gmail.com>
2019-05-04 19:39:11 +0200

    added 3 dots action menu and js toggle

--- app/javascript/packs/project_cards.js
+++ app/javascript/packs/project_cards.js
@@ -1,11 +1,22 @@
 const plusValues = document.querySelectorAll('.project-card-capital-return-15-content');
+const burgerDots = document.querySelectorAll('.card-all-icons-content-dots');
 
-
-const plusValuesShowHide = (event) => {
+const showHidePreviousElement = (event) => {
   event.currentTarget.previousElementSibling.classList.toggle('hidden');
 };
 
+const showHidenextElement = (event) => {
+  event.currentTarget.nextElementSibling.classList.toggle('hidden');
+};
 
+
+// show hide info about plus values
 plusValues.forEach((plusValue) => {
-  plusValue.addEventListener('click', plusValuesShowHide);
+  plusValue.addEventListener('click', showHidePreviousElement);
+});
+
+
+// show hide actions icons
+burgerDots.forEach((burgerDot) => {
+  burgerDot.addEventListener('click', showHidenextElement);
 });
