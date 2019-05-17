eb37359a6068a5d81e95e49abd3a5a832da69ae7
Nicolas Debray <nicdebray@gmail.com>
2019-05-18 00:15:25 +0200

    refactored and extra logic js address suggestion

--- app/javascript/packs/maps_autocomplete.js
+++ app/javascript/packs/maps_autocomplete.js
@@ -4,53 +4,63 @@ const keyApi = 'pk.eyJ1IjoibmljZGViIiwiYSI6ImNqdnBtZnJqNTAwZGszeW1hbWNjbWdkZDAif
 
 // on-page elements
 const searchInput = document.querySelector('.form-address');
-const resultDiv = document.querySelector('.project_address')
+const resultDiv = document.querySelector('.project_address');
 const belgiumPlaces = 0;
 
 
 //check if there's at least one result to display
 const belgiumResults = (results) => {
-  const belgiumArray = []
+  const belgiumArray = new Array;
   results.forEach((result) => {
-    if(result.place_name.includes('Belgium')) {
+    const resultName = result.place_name;
+    if(resultName.includes('Belgium')) {
       belgiumArray.push(result);
     }
   })
   return belgiumArray.length;
-}
+};
 
-const selectedSuggestion = (event) => {
+// set the selected suggestion in the input and clear suggestion
+const setSuggestion = (event) => {
   searchInput.value = event.currentTarget.innerText;
-  const previousResults = document.querySelector('.lu-results');
+  const previousResults = document.querySelector('.ul-results');
   resultDiv.removeChild(previousResults);
+};
 
-}
+// check if there's a <ul>, if so remove it, before reinserting an updated one
+const removeUlPresent = () => {
+  const ulResults = document.querySelector('.ul-results');
+  if (ulResults){
+    resultDiv.removeChild(ulResults);
+  };
+};
+
+// validate there at least one result and then create the <ul>
+const createUl = (results) => {
+  if(belgiumResults(results) > 0) {
+    const ul = document.createElement('ul');
+    ul.classList = 'ul-results card';
+    resultDiv.appendChild(ul);
+  };
+};
 
 // insert results
 const insertResults = (results) => {
-  // check if there's a <lu>, if so remove it, before reinserting an updated one
-  while (document.querySelector('.lu-results')){
-    const previousResults = document.querySelector('.lu-results');
-    resultDiv.removeChild(previousResults);
-  };
-  // validate there at least one result in order to push the ul and its content
-  if(belgiumResults(results) > 0) {
-  const ul = document.createElement('ul');
-  ul.classList = 'lu-results card';
-  resultDiv.appendChild(ul);
+  removeUlPresent();
+  createUl(results);
   results.forEach((result) => {
     if(result.place_name.includes('Belgium')) {
+      const ul = document.querySelector('.ul-results');
       const li = document.createElement('li');
       li.classList = 'resultLi';
       li.innerHTML = result.place_name;
       ul.appendChild(li);
       li.addEventListener('click', (event) => {
-        selectedSuggestion(event);
-      })
-    }
-  })
-}
-}
+        setSuggestion(event);
+      });
+    };
+  });
+};
 
 const getAddresses = (event) => {
   if (event.currentTarget.value != '') {
@@ -62,6 +72,20 @@ const getAddresses = (event) => {
   }
 };
 
+const removeSuggest = () => {
+  document.addEventListener('click', (event) => {
+    const resultSuggestions = document.getElementById('project_address');
+    if(event.currentTarget != resultSuggestions) {
+      removeUlPresent();
+    }
+  });
+};
 
 searchInput.addEventListener('keyup', getAddresses);
 
+// when focus-ing remove suggestions if click outside and
+// when 'refocusing' re-show suggestions based on the current input
+searchInput.addEventListener('focusin', (event) => {
+  removeSuggest();
+  getAddresses(event);
+});
